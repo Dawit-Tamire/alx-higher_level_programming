987d2fc79e25029858eef2f451b2f3e6a90f0534
Dawit Tamire <davtame3@gmail.com>
2022-07-19 13:55:53 -0700

    My first commit

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -61,9 +61,9 @@
          for i in range(self.__position[1]): 
              print() 
          for j in range(self.__size): 
-             print("".join([" " for k in range(self.__position[0])])) 
+             print("".join([" " for k in range(self.__position[0])]), end="") 
              print("".join(["#" for l in range(self.__size)]))
-             print()
+
      @property 
      def position(self): 
          """getter of __position 
